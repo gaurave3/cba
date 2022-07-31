73ea92861b5b29ec66f089767ad850faa8b15974
gaurave3 <110330399+gaurave3@users.noreply.github.com>
2022-07-31 15:25:44 +0530

    Add files via upload

---
+++ GMS.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd
+
+
+# In[11]:
+
+
+pd.read_csv(r'C:\Users\mathe\OneDrive\Documents\Book2.csv')
+
+
+# In[16]:
+
+
+data=pd.read_csv(r'C:\Users\mathe\OneDrive\Documents\Data2.tsv')
+
+
+# In[17]:
+
+
+data
+
+
+# In[ ]:
+
+
+
+
